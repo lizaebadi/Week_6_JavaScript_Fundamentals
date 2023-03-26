ee71cc1a94561970cc80a3a2ae069d7e76e7299b
lizaebadi <liza-1@hotmail.co.uk>
2023-03-26 21:22:08 +0100

    Classes topic

---
+++ phase_1/userBase.js
@@ -0,0 +1,24 @@
+class UserBase {
+
+  constructor(users) {
+    this.users = users;
+  }
+
+  count() {
+    return this.users.length;
+  }
+
+  getNames() {
+    return this.users.map(user => {
+      return user.getName(); 
+    });
+  }
+
+  getIntroductions() {
+    return this.users.map(user => {
+      return user.getIntroduction();
+    });
+  }
+};
+
+module.exports = UserBase;
